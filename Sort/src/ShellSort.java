75aa0ac4c6fd56a7a1bb944adca3e02868a82e23
scofield <liyunan@sinosoft.com.cn>
2020-08-25 17:15:26 +0800

    希尔排序

---
+++ Sort/src/ShellSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void shellSort(int[] arr){
+        int temp = 0;
+        for (int i = arr.length / 2; i > 0 ; i /= 2) {
+            for (int j = i; j < arr.length; j++) {
+                //遍历各组中所有元素（共i组），步长i
+                for (int k = j - i; k >= 0 ; k -= i) {
+                    //如果当前元素大于加上步长后的，元素交换位置
+                    if (arr[k] > arr[k+i]) {
+                        temp = arr[k];
+                        arr[k] = arr[k+i];
+                        arr[k+i] = temp;
+                    }
+                    System.out.println("" + i +"~~~~~~~~~"+ j +"~~~~~~~~~"+ i);
+                }
+            }
+        }
+    }
+}
